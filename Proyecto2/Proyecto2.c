d1380f47c1e488c9232cdd7ea5538f33af81bea7
Pjvl99 <pablovelasquez@ufm.edu>
2020-03-14 12:35:39 -0600

    Fase uno terminada

---
+++ Proyecto2/Proyecto2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int matrices();
+int main(void){
+
+
+
+
+}
+
+int matrices(){
+int matrix[6][6] = {{0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}, 
+{0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0}};
+int y = 0;
+char matrixstr[6][6];
+for(int x = 0; x<6; x++){
+y = 0;    
+for(; y<6; y++){
+printf("%c%d", (65+x), (y+1));
+
+}    
+}
+
+
+}
+
